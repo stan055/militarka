a1abc9d0b7a9ed4f2064bfb1c03003cabfa41579
Stan055 <stanislav055@gmail.com>
2023-04-11 16:28:03 +0300

    111

--- js/checkout.js
+++ js/checkout.js
@@ -1,9 +1,12 @@
+const apiKey = "9bd900f2cabe860e47e323dad85630da";
+const requestURL = 'https://api.novaposhta.ua/v2.0/json/';
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const cart = new Cart();
     renderProductsTable(cart.data);
-
+    // nvstations();
 });
 
 function renderProductsTable(data) {
@@ -31,4 +34,75 @@ function renderProductsTable(data) {
             `;
     });
 }
+
+// City input Google Maps autocomplete 
+let autocomplete;
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+            types: ['locality'],
+            componentRestrictions: {'country': ['UKR']},
+        });
+    autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+        const cityName = place.address_components[0].short_name; 
+        console.log(place);
+        searchSettlements(cityName);
+    });
+    
+}
+
+function searchSettlements(cityName) {
+    const obj = {
+        "apiKey": apiKey,
+        "modelName": "Address",
+        "calledMethod": "searchSettlements",
+        "methodProperties": {
+            "CityName" : "Укр",
+            "Limit" : "50",
+            "Page" : "1"
+        }
+     }
+
+     const request = new Request(requestURL, {
+        method: "POST",
+        body: JSON.stringify(obj),
+      });
+      
+      fetch(request)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch(console.error);
+}
+
+function novaposhtaApi () {
+
+    const obj = {
+       "apiKey": apiKey,
+       "modelName": "Address",
+       "calledMethod": "getWarehouses",
+       "methodProperties": {
+            "CityName" : "українка",
+            "Page" : "1",
+            "Limit" : "50",
+            "Language" : "UA",
+       }
+    }
+   
+    const request = new Request(requestURL, {
+      method: "POST",
+      body: JSON.stringify(obj),
+    });
+    
+    fetch(request)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(console.error);
+}
+
 
