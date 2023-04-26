b5719c7c2d1f546b2e91ce03b12cc3dd6af06964
Stan055 <stanislav055@gmail.com>
2023-04-26 10:59:41 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -198,47 +198,6 @@ function featuredProduct(data) {
     }
 }
 
-// Slider of categories
-// function categorySlider(data) {
-//     const categoriesSlider = document.querySelector('.categories__slider');
-//     categoriesSlider.innerHTML = ``;
-    
-//     Object.entries(data.categories).forEach(category => {
-//         const product = data.products.find(element => element.category === category[0]);
-        
-        
-//         if (product) {
-//             const href=`${document.location.origin}/shop-grid.html?category=${category[0]}`;
-//             categoriesSlider.innerHTML += `
-//                 <div class="col-lg-3">
-//                 <div style="height: 240px; display:flex;align-items: center;">    
-//                 <a href="${href}">
-//                     <img src='${product.imgSrc[0]}'></img>
-//                 </div>
-//                 <a href="${href}">
-//                     <h5 style="text-align: center; padding: 0px; margin: 0px;">${category[1]}</h5>
-//                 </a>
-//                 </div>
-                
-//             `;
-//         }
-//     });
-
-
-
-//     // Owl carousel
-//     $(".categories__slider").owlCarousel({
-//         loop: true, margin: 0, items: 4, dots: false, nav: true,
-//         navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
-//         animateOut: 'fadeOut', animateIn: 'fadeIn', smartSpeed: 1200, autoHeight: false, autoplay: true,
-//         responsive: {
-//             0: { items: 1,},
-//             480: { items: 2,},
-//             768: { items: 3,},
-//             992: { items: 4,}
-//         }});
-// }
-
 // Set Home Hero Banner
 function setHeroBanner(data) {
     const product = data.products.findLast(element => element.category == 'sale');
