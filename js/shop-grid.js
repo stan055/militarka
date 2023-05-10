2519422b337d9363b412494d38300804e1477da5
Stan055 <stanislav055@gmail.com>
2023-05-10 09:28:44 +0300

    fix

--- js/shop-grid.js
+++ js/shop-grid.js
@@ -13,12 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
         hero('hero', data.categories, data.info.tel); // Render menu, search, tel
         footer('footer', data.info)
         
+
         const category = getURLparameter('category');
         if (category != null) {
             const products = data.products.filter(product => product.category == category)
             productsGrid('pagination_container', 'data_container', products)
+            productSort('product_sort', products)
         } else {
             productsGrid('pagination_container', 'data_container', data.products)
+            productSort('product_sort', data.products)
         }
         
         cart = new Cart();
@@ -26,58 +29,9 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 })
 
-// Getting the products
-async function getProducts(category) {
-    try {
-        let data = await getDatabase();
-        data = data.products.filter(product => product.category === category);
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 // Add product by id to shop cart
 function addToCart(id) {
     cart.addToCart(id, productsData);
 }
 
-// display products
-function displayProducts(products) {
-
-    function simpleTemplating(data) {
-        let html = '';
-        $.each(data, function (index, product) {
-            html += `
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background-image: url('${product.imgSrc[0]}');">
-                            <ul class="product__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a style="cursor: pointer;" onclick="addToCart(${product.id})"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="product__item__text">
-                            <h6><a href="#">${product.name}</a></h6>
-                            <h5>$${product.price}</h5>
-                        </div>
-                    </div>
-                </div>
-            `;
-        });
-        return html;
-    };
-    
-    // Pagination
-    $('#pagination-container').pagination({
-        dataSource: productsData,
-        pageSize: 12,
-        callback: function (data, pagination) {
-            // template method of yourself
-            var html = simpleTemplating(data);
-            $('#data-container').html(html);
-        }
-    });
-
-}
---
+++ js/widgets/product-sort.js
@@ -0,0 +1,48 @@
+function productSort(containerId, products) {
+    const container = document.getElementById(containerId)
+    container.innerHTML += `
+    <div class="row">
+    <div class="col-lg-4 col-md-5">
+        <div class="filter__sort">
+            <span>Сортувати</span>
+            <select id="selector_sort">
+                <option value="old">Дата додавання</option>
+                <option value="new">Нове</option>
+                <option value="cheep">Дешеве</option>
+                <option value="expensive">Дороге</option>
+            </select>
+        </div>
+    </div>
+    <div class="col-lg-4 col-md-4">
+        <div class="filter__found">
+            <h6><span>${products.length}</span> Знайдено</h6>
+        </div>
+    </div>
+    <div class="col-lg-4 col-md-3">
+        <div class="filter__option">
+            <span class="icon_grid-2x2"></span>
+            <span class="icon_ul"></span>
+        </div>
+    </div>
+    </div>
+    `
+    const selectorSort = document.getElementById('selector_sort')
+    selectorSort.addEventListener("change", event => {
+        if (event.target.value == 'old') {
+            products = products.sort((a,b)=> a.id - b.id)
+        }
+        if (event.target.value == 'new') {
+            products = products.sort((a,b)=> b.id - a.id)
+        }
+        if (event.target.value == 'cheep') {
+            products = products.sort((a,b)=> a.price - b.price)
+        }
+        if (event.target.value == 'expensive') {
+            products = products.sort((a,b)=> b.price - a.price)
+        }
+        productsGrid('pagination_container', 'data_container', products)
+    });
+}
+
+
+
