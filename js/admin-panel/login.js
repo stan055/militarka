6580ac4f0bddfaed62fa1660bbb720586d504594
Stan055 <stanislav055@gmail.com>
2023-06-16 00:04:29 +0300

    fix

--- js/admin-panel/login.js
+++ js/admin-panel/login.js
@@ -1,18 +1,21 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-
     if (window.parent.length == 1) {
         const login = new Login()
         if (login.user.type == 'admin') window.parent.location.reload() //load to ./admin-panel
         else {
-            const database = new Database();
-            database.getDatabase()
-            .then((data) => {
-                loginForm('login_popup_form', 'login_form_btn', data.login, login)
-            });
+            loginFormDisplay(login)     
         }
     } 
     else {
         window.location.href = './' //load to ./admin-panel  
     }  
 })
+
+function loginFormDisplay(login) {
+    const database = new Database();
+    database.getDatabase()
+    .then((data) => {
+        loginForm('login_popup_form', 'login_form_btn', data.login, login)
+    });
+}
